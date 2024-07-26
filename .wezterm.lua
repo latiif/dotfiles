246e73d34f3d8fe9dd59b8c8bd2ec3568b94af25
Latiif <latiif@mail.com>
2024-07-26 16:44:26 +0200

    Specify a mono nerd font

--- .wezterm.lua
+++ .wezterm.lua
@@ -6,7 +6,7 @@ local config = wezterm.config_builder()
 
 config.color_scheme = 'Mocha (light) (terminal.sexy)'
 config.hide_tab_bar_if_only_one_tab = true
-config.font = wezterm.font('Cascadia Code')
+config.font = wezterm.font('FiraMono Nerd Font Mono')
 config.font_size = 12
 config.font_shaper = "Harfbuzz"
 
